f0873cbf0b3f32c689cbf6f085383b6effdd3557
BaHwan Han <beingbook@gmail.com>
2019-03-04 00:13:22 +0900

    isolate component thiggy

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,16 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 
-import Header from './Header';
-import './Layout.css';
+import './styles.css';
+import Header from '../Header';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -32,7 +25,8 @@ const Layout = ({ children }) => (
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
-          }}>
+          }}
+        >
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
