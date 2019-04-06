d28f540a78a1172d09f8a2b589675e2d3f166c7c
BaHwan Han <beingbook@gmail.com>
2019-04-06 17:22:49 +0900

    add jss sheet

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,36 @@
 import React from 'react';
-import MuiProvider from './src/container/MuiProvider';
+import { renderToString } from 'react-dom/server';
+import MuiProvider, { getPageContext } from './src/container/MuiProvider';
 
-export const withRootElement = ({ element }) => {
-  return <MuiProvider> {element} </MuiProvider>;
+const withRootElement = ({ element }) => {
+  return <MuiProvider>{element}</MuiProvider>;
 };
+
+const replaceRenderer = ({
+  bodyComponent,
+  replaceBodyHTMLString,
+  setHeadComponents,
+}) => {
+  // Get the context of the page to collected side effects.
+  const muiPageContext = getPageContext();
+
+  const bodyHTML = renderToString(
+    <JssProvider registry={muiPageContext.sheetsRegistry}>
+      {bodyComponent}
+    </JssProvider>,
+  );
+
+  replaceBodyHTMLString(bodyHTML);
+  setHeadComponents([
+    <style
+      type="text/css"
+      id="jss-server-side"
+      key="jss-server-side"
+      dangerouslySetInnerHTML={{
+        __html: muiPageContext.sheetsRegistry.toString(),
+      }}
+    />,
+  ]);
+};
+
+export { withRootElement, replaceRenderer };
